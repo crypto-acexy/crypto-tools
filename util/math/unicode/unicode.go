0157fc70e4f6c5399b1f07fa26b044bc946c0f77
Acexy <thankjava@gmail.com>
2024-03-21 14:20:31 +0800

    update

--- util/math/unicode/unicode.go
+++ util/math/unicode/unicode.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"unicode/utf8"
 )
 
@@ -16,10 +15,12 @@ func main() {
 	char, _ := utf8.DecodeRuneInString(os.Args[1])
 	encoded := make([]byte, utf8.UTFMax)
 	size := utf8.EncodeRune(encoded, char)
-	result := fmt.Sprintf("%08b", encoded[:size])
-	result = strings.ReplaceAll(result, "[", "")
-	result = strings.ReplaceAll(result, "]", "")
+	binary := fmt.Sprintf("%08b", encoded[:size])
+	decimal := fmt.Sprint(encoded[:size])
+	binary = binary[1 : utf8.RuneCountInString(binary)-1]
+	decimal = decimal[1 : len([]rune(decimal))-1]
 	unicode := fmt.Sprintf("u+%04x", char)
 	fmt.Printf("Unicode: %s , %d\n", unicode, char)
-	fmt.Println("Binary code:", result)
+	fmt.Println("Binary code:", binary)
+	fmt.Println("Decimals:", decimal)
 }
